a412e3ebc5300c69874e303c42cfdc34c04e8835
PapiChuloXX <afrokid010404@gmail.com>
2023-04-21 12:50:10 -0500

    a

---
+++ geopy.py
@@ -0,0 +1,10 @@
+from geopy.geocoders import GoogleV3
+import geopy.distance
+import googlemaps
+
+geolocator = GoogleV3(api_key="AIzaSyAOlXE709gmbTW4gPyVGCxS33Px4pYlnGo")
+place = "Old Trafford"
+location = geolocator.geocode(place)
+
+print(location.address)
+print(location.latitude, location.longitude)
