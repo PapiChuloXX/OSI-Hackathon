f94b620e45f26b37c28c5bc8d04b3c9b597bf13a
PapiChuloXX <afrokid010404@gmail.com>
2023-04-22 13:17:25 -0500

    c

--- geopy6.py
+++ geopy6.py
@@ -1,10 +1,14 @@
-from geopy.geocoders import GoogleV3
-import geopy.distance
-import googlemaps
+import ephem # pip install pyephem (on Python 2)
+             # pip install ephem   (on Python 3)
+def main():
+    observer = ephem.Observer()
+    observer.lat, observer.long = 45.5579451, -94.16324039999999
+    print(solartime(observer=observer))
 
-geolocator = GoogleV3(api_key="AIzaSyAOlXE709gmbTW4gPyVGCxS33Px4pYlnGo")
-place = "St. Cloud"
-location = geolocator.geocode(place)
+def solartime(observer, sun=ephem.Sun()):
+    sun.compute(observer)
+    # sidereal time == ra (right ascension) is the highest point (noon)
+    hour_angle = observer.sidereal_time() - sun.ra
+    return ephem.hours(hour_angle + ephem.hours('12:00')).norm  # norm for 24h
 
-print(location.address)
-print(location.latitude, location.longitude)
+main()
